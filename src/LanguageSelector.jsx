05252f0677926b859723444737625d5cf550a46e
Thaotic-good <fu3.martina@gmail.com>
2024-03-12 14:50:05 +0100

    LanguageSelector.jsx finished

--- src/LanguageSelector.jsx
+++ src/LanguageSelector.jsx
@@ -1,8 +1,4 @@
 import {useState} from "react";
-/*1. I have a list of languages, I want to use .map to create a <li> for each element
-* 2. I want to create a function, to attach to each element, if I click on the <li> it will be added to a different
-*    <ul> consisting of only the selected languages and the given language turns green
-* 3. clicking again on the language again will unselect the language out of the list of selected items and not be green anymore*/
 
 function LanguageSelector(){
     const [languages] = useState(["JavaScript", "Python", "Java", "Bash", "Kotlin"])
@@ -10,14 +6,16 @@ function LanguageSelector(){
 
     const handleClickSelection = (language)=> {
         const isInList = selected.includes(language)
-        if (isInList) {
-            const languageIndex = selected.indexOf(language)
-            setSelected(prevSelected => prevSelected.splice(languageIndex,1))
-        }
-        else setSelected(prevSelected  =>  [...prevSelected, language])
+       if (isInList) {
+           const languageIndex = selected.indexOf(language)
+           setSelected(prevSelected=> prevSelected.splice(languageIndex,1))
+           /*=> prevSelected is for when I don't want to rewrite the whole state also
+           updates to state are scheduled and don't happen instantly but during re-render*/
+       }
+        else setSelected(prevSelected => [...prevSelected, language])
     }
 
-    return (
+    return(
         <>
             <ul>
                 {languages.map((language, index) =>
